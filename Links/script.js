c0168b648fc894158bf4f624e6e3a8cc9156e771
Bianca G. Griffin <Biancagriffin@hotmail.es>
2024-04-08 12:10:10 +0200

    actualizado3

---
+++ Links/script.js
@@ -0,0 +1,81 @@
+const addLinkButton = document.getElementById("addLinkButton");
+const nameInput = document.getElementById("name-input");
+const urlInput = document.getElementById("url-input");
+const linksStorage = document.getElementById("linksStorage");
+
+//funcionamiento de añadir Links
+//persistencia links
+//funcionamiento eliminar link-links
+
+//agregar un evento al boton, cuando introducimos datos en input
+//function para almacenar en localstorage y convertir en obg/array key y value
+//function para createElement li,ahref
+
+addLinkButton.addEventListener("click", function () {
+  const nameLink = nameInput.value;
+  const nameUrl = urlInput.value;
+  //console.log(nameLink)
+  if (nameLink === "" || nameUrl === "") {
+    alert("Please fill NAME||LINK");
+    return;
+  }
+  addLinksSaved(nameLink, nameUrl); //agregamos function
+  addLinksDom(); //agregamos a la function
+});
+
+//creo function con parametros y crear key/value en locasStorage
+//creo function con parametros name/url para almacenar localStorage
+//obtengo valor asociado a la clave "links-url"/convierto cadena JSON en JASON.p=obj
+//metodo llamada del valor 'links-url' y se convierte en json que representa datos array;
+//convertimos link en cadena Json y guardamos localStorage con key links-url
+function addLinksSaved(name, url) {
+  let links = JSON.parse(localStorage.getItem("links-url")) || [];
+  links.push({ name: name, url: url });
+  localStorage.setItem("links-url", JSON.stringify(links));
+}
+//addLinksSaved()
+
+//crear function para añadir dom???
+//eliminar nodo hijos
+//link es el elemento actual del array y en cada iteracion, link tomara nombre de uno de los elemntos del array.
+//indice del elemento actual del array links, itera desde 0 a -1???????
+
+function addLinksDom() {
+  let links = JSON.parse(localStorage.getItem("links-url")) || [];
+  linksStorage.innerHTML = "";
+  //itera sobre cada elemento del array links//indice lo utilizamos boton LOCO
+  links.forEach((link, indice) => {
+    const li = document.createElement("li");
+    li.classList.add("line");
+    const a = document.createElement("a");
+    a.classList.add("ahref");
+    a.href = link.url; //tomando propiedad
+    a.textContent = link.name; //establecer
+    li.appendChild(a); //creamos appenChild donde a es hijo de li........
+
+    //aqui añado boton LOCO borrar/CN
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("BtnErase");
+    deleteBtn.textContent = "X";
+
+    deleteBtn.addEventListener("click", function () {
+      eraseBtn(indice);
+      addLinksDom(); // actualizar reflejar cambios en li/a/textContent
+    });
+    // console.log()
+    // addLinksDom()
+    li.appendChild(deleteBtn); // hijo para que se muestre
+    linksStorage.appendChild(li);
+  });
+}
+//addLinksDom();
+
+//hacer funcionar al boton erase / que tiene que hacer para añadir a todos los enlaces....
+//ponemos parametro forEach indice/eliminar enlaces almacenados basado en indice
+// metodo splice del array linksStored y pasamos argumento indice, elimina todos los elementos del array apartir del indice
+const eraseBtn = (indice) => {
+  let linksStored = JSON.parse(localStorage.getItem("links-url"));
+  //si no agrego 1 , se borra todo cuando pulsamos el primer enlace a/CN
+  linksStored.splice(indice, 1);
+  localStorage.setItem("links-url", JSON.stringify(linksStored));
+};
--- estación meteorológica/script.js
+++ estación meteorológica/script.js
@@ -1,1 +1,72 @@
-console.log('hello world!')
+const currentWeather = document.getElementById("currentWeather");
+const locationH2 = document.getElementById("location");
+const conditionP = document.getElementById("condition");
+const Precipitation = document.getElementById("precipitation");
+const wind = document.getElementById("wind");
+const Humidity = document.getElementById("humidity");
+const temperature = document.querySelector(".temperature");
+const iconWeather = document.getElementById("iconWeather");
+const weatherIconsPic = document.getElementById("weatherIcons");
+const smallSquareGrades = document.querySelectorAll(".smallSquareGrades");
+const baseUrl = "http://api.weatherapi.com/v1";
+
+
+
+//obtener datos weather de API
+const getWeatherApi = () => {
+  fetch(
+    "https://api.weatherapi.com/v1/current.json?key=5900cbebf0de4c56930190657240604&q=sevilla&aqi=no#"
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("not working at the moment");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      getKnowWeather(data); //cambiar console.log x getKnowWeather para DOM.
+    })
+    .catch((error) => {
+      console.log("error to obtain weather;", error);
+    });
+};
+
+getWeatherApi();
+
+//sacar dom datos JSON
+const getKnowWeather = (data) => {
+  locationH2.textContent = `${data.location.name}, ${data.location.country}`;
+  temperature.textContent = `${data.current.temp_c}ºc`;
+  weatherIconsPic.src = `${data.current.condition.icon}`;
+  conditionP.textContent = data.current.condition.text;
+  Precipitation.textContent = `Precipitacion: ${data.current.precip_mm}`;
+  Humidity.textContent = `Humidity: ${data.current.humidity}`;
+  wind.textContent = `wind: ${data.current.wind_kph}`;
+  tempC.textContent = `${data.current.temp_c}ºc`; //probando
+   
+
+  
+};
+//console.log()
+
+
+const getIcons = async () => {
+  try {
+    const response = await fetch(
+      "https://api.weatherapi.com/v1/forecast.json?key=5900cbebf0de4c56930190657240604&q=sevilla&days=1&aqi=no&alerts=no"
+    );
+    if (!response.ok) {
+      throw new Error("Getting hour not working!");
+    }
+    const data = await response.json();
+    const infoApi = data.forecast.forecastday[0].hour
+    // const infoIcon = data.forecast.forecastday[0].condition.icon
+    // const infoC = data.forecast.forecastday[0].temp_c
+    
+   console.log(infoApi)
+  } catch (error) {
+    console.error("Error getting time:", error);
+  }
+};
+
+getIcons();
